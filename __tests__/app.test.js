ced3fdc50f150db851df17543c63553e00d0c9f0
sirdan <stanford303@gmail.com>
2022-08-12 09:37:42 -0700

    redirect user test in

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,45 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+ const request = require('supertest');
+ const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+ jest.mock('../lib/services/github');
+
+
+
+describe('oauth routes', () => {
   beforeEach(() => {
     return setup(pool);
+    
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should login in and redirect user to dash board', async () => {
+    const res = await request
+    .agent(app)
+    .get('/api/v1/github/callback?code=42')
+    .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      userName: 'fake_github_user',
+      email: '100@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+
+    });
   });
+
+
+
+
+
+
+
+
+
+
+
+
   afterAll(() => {
     pool.end();
   });
